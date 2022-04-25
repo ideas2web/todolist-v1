cdc9186d23301b88fbd43a92122cc9377b58facc
Akinlade Adesina <akin4true@gmail.com>
2022-04-25 20:56:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//jshint esversion:6
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res){
+  var today = new Date();
+
+  if(today.getDay() === 6 || today.getDay() === 0) {
+    res.send("Yay It's the weekend!");
+  }else{
+    res.send("Boo! I have to work!");
+  }
+
+});
+
+app.listen(3000, function(){
+  console.log("server started on port 3000");
+});
