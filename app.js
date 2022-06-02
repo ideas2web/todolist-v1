0f0d55634e766658f4787f53bb940ac9fb77de70
Akinlade Adesina <66315685+ideas2web@users.noreply.github.com>
2022-06-02 12:05:37 +0100

    css-applied

--- app.js
+++ app.js
@@ -6,17 +6,40 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({entended: true}));
+app.use(express.static("public"));
+
 app.get("/", function(req, res){
+
   var today = new Date();
 
-  if(today.getDay() === 6 || today.getDay() === 0) {
-    res.send("Yay It's the weekend!");
-  }else{
-    res.send("Boo! I have to work!");
-  }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-});
+  var day = today.toLocaleDateString("en-US", options);
 
+res.render("list", {
+  kindOfDay: day,
+  newListItems: items
+  });
+
+  app.post("/", function(req, res) {
+   item = req.body.newItem;
+   items.push(item);
+
+  res.redirect("/");
+  });
+
+
+  });
 app.listen(3000, function(){
   console.log("server started on port 3000");
 });
